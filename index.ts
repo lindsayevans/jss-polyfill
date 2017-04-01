bd6d1da54642bb336fa4858ade2d58970167f8f4
Lindsay Evans <lindsay@boomworks.com.au>
2017-04-01 18:29:52 +1100

    Adding support for ID & class style collections

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 // TODO:
 // * Feature detection (bail if 'tags' in document)
 // * Add support for with (document.tags) syntax
-// * Add support for other style collections (classes, ids)
 // * Add support for inline styles (<P STYLE="color = 'green'">)
 // * Add support for pseudo classes ('Typographical Elements' in proposal)
 // * Normalise property names - camelCase to kebab-case, expand shorthand properties (e.g. bgColor)
@@ -12,10 +11,13 @@
 let $stylesheet = document.querySelector('style[type="text/javascript"]');
 let styles = $stylesheet.innerHTML.trim();
 
-// Add empty tags style collection to document
+// Add empty style collections to document
 document.tags = {};
+document.ids = {};
+document.classes = {};
 
 // Proxy to initialise undefined properties
+// TODO: Create a factory or something to handle this cruft
 let tagsProxy = new Proxy<any>(document.tags, {
     get: (obj, prop) => {
 
@@ -27,9 +29,32 @@ let tagsProxy = new Proxy<any>(document.tags, {
         return obj[prop];
     }
 });
+let idsProxy = new Proxy<any>(document.ids, {
+    get: (obj, prop) => {
+
+        if (document.ids[prop] === undefined) {
+            document.ids[prop] = {};
+        }
+
+        return obj[prop];
+    }
+});
+let classesProxy = new Proxy<any>(document.classes, {
+    get: (obj, prop) => {
+
+        if (document.classes[prop] === undefined) {
+            document.classes[prop] = {};
+        }
+
+        return obj[prop];
+    }
+});
 
 // Replace document.X assignments with calls to proxy
-styles = styles.replace(/document.tags/gi, 'tagsProxy');
+styles = styles
+    .replace(/document.tags/gi, 'tagsProxy')
+    .replace(/document.ids/gi, 'idsProxy')
+    .replace(/document.classes/gi, 'classesProxy');
 
 // Evaluate is now considered sexy
 eval(styles);
@@ -37,10 +62,27 @@ eval(styles);
 // Build CSS
 let cssStyles = '';
 
+// TODO: Refactor
 for (let tagName in document.tags) {
     cssStyles += `${tagName} {`;
     for (let propertyName in document.tags[tagName]) {
         cssStyles += `${propertyName}: ${document.tags[tagName][propertyName]};`;
+    }
+    cssStyles += '}';
+}
+
+for (let id in document.ids) {
+    cssStyles += `#${id} {`;
+    for (let propertyName in document.ids[id]) {
+        cssStyles += `${propertyName}: ${document.ids[id][propertyName]};`;
+    }
+    cssStyles += '}';
+}
+
+for (let className in document.classes) {
+    cssStyles += `.${className} {`;
+    for (let propertyName in document.classes[className]) {
+        cssStyles += `${propertyName}: ${document.classes[className][propertyName]};`;
     }
     cssStyles += '}';
 }
